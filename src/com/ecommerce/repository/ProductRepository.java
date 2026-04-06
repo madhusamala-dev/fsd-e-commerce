ad8007b3cc71b5d4fa3881232f95f496e2dd3937
madhusamala-dev <madhu@trainingmug.com>
2026-04-06 13:04:31 +0530

    feat: ProductRepoistory with basic CRUD

--- src/com/ecommerce/repository/ProductRepository.java
+++ src/com/ecommerce/repository/ProductRepository.java
@@ -1,8 +1,39 @@
 package com.ecommerce.repository;
 
 
+import com.ecommerce.model.Product;
+import com.ecommerce.util.CsvParser;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 
 public class ProductRepository {
+    /*
+    CRUD operations on Products
+     */
+    private final List<Product> products;
+
+
+    ProductRepository() throws IOException {
+        CsvParser csvParser = new CsvParser();
+        products = csvParser.getProductsFromCsv();
+    }
+
+    public Product save(Product product) {
+        this.products.add(product);
+        return product;
+    }
+
+    public Optional<Product> getById(int id) {
+        return products.stream()
+                .filter(product -> product.getId() == id)
+                .findFirst();
+    }
+
+    public List<Product> getAll(){
+        return this.products;
+    }
 
 
 
